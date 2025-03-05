3be2f6e6b24717173bf7b92d2f3c5c72ececb23c
Ujjwal Arora <ukarora9@gmail.com>
2025-03-05 22:51:20 +0530

    Initial Commit

---
+++ XCodeCloudWesChua/XCodeCloudWesChuaApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeCloudWesChuaApp.swift
+//  XCodeCloudWesChua
+//
+//  Created by Ujjwal Arora on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct XCodeCloudWesChuaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
